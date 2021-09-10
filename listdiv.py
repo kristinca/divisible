6c26365f6a4802d66bcf76c54c3f2768d87ee770
Kristina Jovanovska <72104951+kristinca@users.noreply.github.com>
2021-09-10 20:54:17 +0200

    checking divisibility of input elements of a list without the modulo operator

---
+++ listdiv.py
@@ -0,0 +1,106 @@
+"""
+
+A simple program to check if some input elements of a list
+         are divisible by some input integer
+          without using the modulo operator,
+          atm it exits at the first found exception,
+          that should be fixed
+
+"""
+
+import sys
+
+
+def nasterisk(n):
+    """ A function that returns n asterisks """
+
+    return(int(n) * '*')
+
+
+def div1(num1, num2):
+    """
+    A simple function to check if an input integer is divisible by another input integer
+                        without using the modulo operator
+    """
+    try:
+        first_number = num1
+        second_number = num2
+        answer = int(first_number) / int(second_number)
+        if answer.is_integer():
+            print(f'\nThe number {first_number} is divisible by the number {second_number},'
+                  f'the result is : {int(answer)}.')
+        else:
+            print(f'\nThe number {first_number} is not divisible by the number {second_number}.')
+
+    except ZeroDivisionError:
+        """ We handle the ZeroDivisionError """
+        m1 = ' Zero division! '
+        print(f"\n{nasterisk(len(m1))}\n{m1}\n{nasterisk(len(m1))}")
+
+    except ValueError:
+        """ We handle the ValueError """
+        m2 = ' At least one of the numbers is not an integer! '
+        print(f"\n{nasterisk(len(m2))}\n{m2}\n{nasterisk(len(m2))}")
+
+
+def fin(s):
+    """ A simple function to end the program when entered 'q' """
+
+    if s == 'q':
+        sys.exit()
+    pass
+
+
+
+def msginfo():
+    """ A simple output messages to be printed when handling exceptions """
+
+    m1 = ' Inappropriate argument type or inappropriate argument value! '
+    m2 = " Enter 'q' to exit. "
+    print(f'\n{nasterisk(len(m1))}\n{m1}\n{nasterisk(len(m1))}')
+    print(f"\n{nasterisk(len(m2))}\n{m2}\n{nasterisk(len(m2))}")
+    # pass
+
+
+while True:
+    """ The program ends by entering 'q' """
+    try:
+        m1 = ' S T A R T '
+        print(f'\n {nasterisk(len(m1))}{m1}{nasterisk(len(m1))} \n')
+        list1 = []
+        n1 = input('\nEnter the number of elements in the list...\t')
+        fin(n1)
+        if int(n1) <= 0:
+            msginfo()
+        else:
+            for i in range(0, int(n1)):
+                list1.append(input(f'\nEnter the element with index value {i} ... \t'))
+            n2 = input(f"\nEnter the number of elements you want to check for divisibility from list {list1}"
+                       f" or enter 'q' to exit...\t")
+            fin(n2)
+            if int(n2) <= 0 or int(n2) > len(list1):
+                pass
+            else:
+                n3 = []
+                for i in range(0, int(n2)):
+                    n3.append(input('\nEnter the index of the element to check for divisibility...\t'))
+                d = input("\nEnter the divisor or enter 'q' to exit...\t")
+                for j in list(dict.fromkeys(n3)):
+                    try:
+                        div1(int(list1[int(j)]), int(d))
+                    except IndexError:
+                        m3 = f" There is no element with index {j} in {list1} "
+                        print(f'\n{nasterisk(len(m3))}\n{m3}.\n{nasterisk(m3)}\n')
+                    except TypeError:
+                        m4 = f" Inappropriate argument type: {list1[j]}. "
+                        print(f'\n{nasterisk(len(m4))}\n{m4}\n{nasterisk(m4)}\n')
+                        # pass
+                    except ValueError:
+                        m5 = f" Inappropriate argument value: {list1[j]}. "
+                        print(f'\n{nasterisk(len(m5))}\n{m5}\n{nasterisk(len(m5))}\n')
+                        # pass
+                print("\nEnter 'q' to exit.")
+    except TypeError:
+        msginfo()
+    except ValueError:
+        msginfo()
